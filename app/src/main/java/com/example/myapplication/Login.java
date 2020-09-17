e0a79de2a74c3cb489c311ba716b884e500d8c0c
VISHAL GAUR <57058981+gaursuniverse@users.noreply.github.com>
2020-09-18 04:02:00 +0530

    final commit

---
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -0,0 +1,24 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //This Line will hide the status bar from the screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void registerUser(View view) {
+        startActivity(new Intent(getApplicationContext(),SignUp.class));
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void loginUser(View view) {
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
+    }
+}
